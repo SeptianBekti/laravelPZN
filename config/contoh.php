e15800b4ecf513841b7949428e4900344126b6a1
Septian Bekti <septianbekti99@gmail.com>
2023-04-14 13:54:33 +0700

    EnvirontmentTest

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "name" => [
+        "first" => env('NAME_FIRST', "Eko"),
+        "last" => env('NAME_LAST',"Khannedy" )
+    ],
+    "email" => "echo.khannedy@gmail.com",
+    "web" => "https://www.programmerzamannow.com"
+];
